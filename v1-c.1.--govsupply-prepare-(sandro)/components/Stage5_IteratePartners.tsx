b9f84e28423a7fe733110ee5ed85e86ec4ed0e39
sandropegoraro570-ai <sandropegoraro570@gmail.com>
2025-10-28 12:37:50 +0100

    Delete v1-c.1.--govsupply-prepare-(sandro) directory

--- v1-c.1.--govsupply-prepare-(sandro)/components/Stage5_IteratePartners.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Stage5IteratePartners: React.FC = () => {
-  return null;
-};
-
-export default Stage5IteratePartners;
--- v1-c.1.--govsupply-prepare-(sandro)/components/Stage6_ValidateProposal.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Stage6ValidateProposal: React.FC = () => {
-  return null;
-};
-
-export default Stage6ValidateProposal;
